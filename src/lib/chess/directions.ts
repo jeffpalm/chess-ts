022011cbaaa971a0b9998356c27f7fb935dcd5af
Jeff Palmer <jeffp@covr.care>
2022-05-09 18:33:39 -0400

    cleaned up directions

--- src/lib/chess/directions.ts
+++ src/lib/chess/directions.ts
@@ -1,6 +1,4 @@
-import { Square } from './board/square'
-import { MovePayload, PotentialMove } from './move'
-import { Dir } from 'fs'
+import { PotentialMove } from './move'
 import { IPiece } from './pieces/piece'
 import { SquareName, SquarePosition } from './board/board'
 
@@ -445,76 +443,3 @@ export const directions = (piece: IPiece): Directions => ({
     }),
   ],
 })
-
-export const north: Square[] = [
-  new Square({ y: 0, x: 1 }),
-  new Square({ y: 0, x: 2 }),
-  new Square({ y: 0, x: 3 }),
-  new Square({ y: 0, x: 4 }),
-  new Square({ y: 0, x: 5 }),
-  new Square({ y: 0, x: 6 }),
-  new Square({ y: 0, x: 7 }),
-]
-export const south: Square[] = [
-  new Square({ y: 0, x: 6 }),
-  new Square({ y: 0, x: 5 }),
-  new Square({ y: 0, x: 4 }),
-  new Square({ y: 0, x: 3 }),
-  new Square({ y: 0, x: 2 }),
-  new Square({ y: 0, x: 1 }),
-  new Square({ y: 0, x: 0 }),
-]
-export const east: Square[] = [
-  new Square({ y: 1, x: 0 }),
-  new Square({ y: 2, x: 0 }),
-  new Square({ y: 3, x: 0 }),
-  new Square({ y: 4, x: 0 }),
-  new Square({ y: 5, x: 0 }),
-  new Square({ y: 6, x: 0 }),
-  new Square({ y: 7, x: 0 }),
-]
-export const west: Square[] = [
-  new Square({ y: 6, x: 0 }),
-  new Square({ y: 5, x: 0 }),
-  new Square({ y: 4, x: 0 }),
-  new Square({ y: 3, x: 0 }),
-  new Square({ y: 2, x: 0 }),
-  new Square({ y: 1, x: 0 }),
-  new Square({ y: 0, x: 0 }),
-]
-export const northEast: Square[] = [
-  new Square({ y: 1, x: 1 }),
-  new Square({ y: 2, x: 2 }),
-  new Square({ y: 3, x: 3 }),
-  new Square({ y: 4, x: 4 }),
-  new Square({ y: 5, x: 5 }),
-  new Square({ y: 6, x: 6 }),
-  new Square({ y: 7, x: 7 }),
-]
-export const northWest: Square[] = [
-  new Square({ y: 1, x: 6 }),
-  new Square({ y: 2, x: 5 }),
-  new Square({ y: 3, x: 4 }),
-  new Square({ y: 4, x: 3 }),
-  new Square({ y: 5, x: 2 }),
-  new Square({ y: 6, x: 1 }),
-  new Square({ y: 7, x: 0 }),
-]
-export const southEast: Square[] = [
-  new Square({ y: 6, x: 1 }),
-  new Square({ y: 5, x: 2 }),
-  new Square({ y: 4, x: 3 }),
-  new Square({ y: 3, x: 4 }),
-  new Square({ y: 2, x: 5 }),
-  new Square({ y: 1, x: 6 }),
-  new Square({ y: 0, x: 7 }),
-]
-export const southWest: Square[] = [
-  new Square({ y: 6, x: 6 }),
-  new Square({ y: 5, x: 5 }),
-  new Square({ y: 4, x: 4 }),
-  new Square({ y: 3, x: 3 }),
-  new Square({ y: 2, x: 2 }),
-  new Square({ y: 1, x: 1 }),
-  new Square({ y: 0, x: 0 }),
-]
